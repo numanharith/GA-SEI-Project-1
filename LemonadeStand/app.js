eb9a903012e2ed91c89a5d61e295c84ed778c2b2
numan <numan.h.99@gmail.com>
2021-01-13 16:21:51 +0800

    modified game to work within a single html page

--- LemonadeStand/app.js
+++ LemonadeStand/app.js
@@ -1,37 +1,63 @@
-class Cup {
-    constructor () {
-        this.costFor25 = 0.96;
-        this.costFor50 = 1.6;
-        this.costFor100 = 3;
-    }
-}
-class IceCubes {
-    constructor () {
-        this.costFor100 = 0.98;
-        this.costFor250 = 2.15;
-        this.costFor500 = 3.7;
-    }
-}
-var storedItem = localStorage.getItem("storedItem"); 
+let cupSupplies = 0;
+let iceCubeSupplies = 0;
+let cash = 20;
 
-let numberOfCups = 0;
+const displayCupSupplies = document.getElementById("numberofcups"); 
+const displayIceCubeSupplies = document.getElementById("numberoficecubes"); 
+const displayCash = document.getElementById("cash");
+
+function boughtCups(noOfCupsBought, cost) {
+        cupSupplies += noOfCupsBought;
+        cash -= cost;
+        displayCupSupplies.innerHTML = cupSupplies ;
+        displayCash.innerHTML = cash.toFixed(2);
+}
+
+function boughtIceCubes(noOfIceCubesBought, cost) {
+        iceCubeSupplies += noOfIceCubesBought;
+        cash -= cost;
+        displayIceCubeSupplies.innerHTML = iceCubeSupplies;
+        displayCash.innerHTML = cash.toFixed(2);
+}
+
 function buy25cups() {
-    var Item = numberOfCups++;
-    localStorage.setItem("storedItem", Item);
-    document.getElementById('savedText').innerHTML = Item + " SAVED";
+    if (cash > .96) {
+        boughtCups(25, 1.2);
+    }
 }
 
-function get() {
-    localStorage.getItem("storedItem");
-    document.getElementById("openedText").innerHTML = storedItem + " OPENED";
+function buy50cups() {
+    if (cash > 1.6) {
+        boughtCups(50, 3.14)
+    }
 }
 
-const iceCubes = new IceCubes();
-const cup = new Cup();
-
-function populateStorage() {
-    localStorage.setItem('')
+function buy100cups() {
+    if (cash > 3.23) {
+        boughtCups(100, 5.23)
+    }
 }
+
+function buy100icecubes() {
+    if (cash > .98) {
+        boughtIceCubes(100, .98);
+    }
+}
+
+function buy250icecubes() {
+    if (cash > 2.15) {
+        boughtIceCubes(250, 2.15)
+    }
+}
+
+function buy500icecubes() {
+    if (cash > 3.7) {
+        boughtIceCubes(500, 3.7)
+    }
+}
+
+
+
 $(() => {
     // const buyCups = () => {
     //     let numberOfCups = 0;
@@ -59,26 +85,26 @@ $(() => {
     //     });
     // }
 
-    const buyIceCubes = () => {
-        $('#buy100cubes-btn').on('click', () => {
-            if (parseInt($('.cash').html()) > iceCubes.costFor100) {
-                $('.numberoficecubes').html(parseInt($('.numberoficecubes').html()) + 100);
-                $('.cash').html((parseFloat($('.cash').html()) - iceCubes.costFor100).toFixed(2));
-            }
-        });
-        $('#buy250cubes-btn').on('click', () => {
-            if (parseInt($('.cash').html()) > iceCubes.costFor250) {
-                $('.numberoficecubes').html(parseInt($('.numberoficecubes').html()) + 250);
-                $('.cash').html((parseFloat($('.cash').html()) - iceCubes.costFor250).toFixed(2));
-            }
-        });
-        $('#buy500cubes-btn').on('click', () => {
-            if (parseInt($('.cash').html()) > iceCubes.costFor500) {
-                $('.numberoficecubes').html(parseInt($('.numberoficecubes').html()) + 500);
-                $('.cash').html((parseFloat($('.cash').html()) - iceCubes.costFor500).toFixed(2));
-            }
-        });
-    }
-    
-    buyIceCubes();
+    // const buyIceCubes = () => {
+    //     $('#buy100cubes-btn').on('click', () => {
+    //         if (parseInt($('.cash').html()) > iceCubes.costFor100) {
+    //             $('.numberoficecubes').html(parseInt($('.numberoficecubes').html()) + 100);
+    //             $('.cash').html((parseFloat($('.cash').html()) - iceCubes.costFor100).toFixed(2));
+    //         }
+    //     });
+    //     $('#buy250cubes-btn').on('click', () => {
+    //         if (parseInt($('.cash').html()) > iceCubes.costFor250) {
+    //             $('.numberoficecubes').html(parseInt($('.numberoficecubes').html()) + 250);
+    //             $('.cash').html((parseFloat($('.cash').html()) - iceCubes.costFor250).toFixed(2));
+    //         }
+    //     });
+    //     $('#buy500cubes-btn').on('click', () => {
+    //         if (parseInt($('.cash').html()) > iceCubes.costFor500) {
+    //             $('.numberoficecubes').html(parseInt($('.numberoficecubes').html()) + 500);
+    //             $('.cash').html((parseFloat($('.cash').html()) - iceCubes.costFor500).toFixed(2));
+    //         }
+    //     });
+    // }
+
+    // buyIceCubes();
 });
